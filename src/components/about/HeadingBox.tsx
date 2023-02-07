c4a94165433e971e738f1eeb6585f4701f46bcb9
likelionbootcamp3 <likelionbootcamp3@gmail.com>
2023-02-07 17:15:09 +0700

    fix: update HeadingBox in about

--- src/components/about/HeadingBox.tsx
+++ src/components/about/HeadingBox.tsx
@@ -6,7 +6,9 @@ export interface HeadingBoxProps {
 const HeadingBox = ({ heading, text }: HeadingBoxProps) => {
   return (
     <div className="max-w-md mx-auto mb-12 text-center">
-      <h2 className="text-4xl font-bold text-black capitalize">{heading}</h2>
+      <h2 className="text-3xl font-bold text-black capitalize md:text-4xl">
+        {heading}
+      </h2>
       <p className="mt-4 leading-relaxed text-body-light">{text}</p>
     </div>
   );
